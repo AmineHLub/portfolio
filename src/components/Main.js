3cda0d37b1782c4c9bed51967205c101fd0c4ed9
AmineHLub <Amino.bhl@gmail.com>
2022-02-25 22:12:23 +0100

    Concern seperation

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,40 +1,10 @@
 import '../mobile-styling/main.css';
-import myPic from '../assets/photo.png';
-import angel from '../assets/socials/angel.png';
-import github from '../assets/socials/github.png';
-import linkedin from '../assets/socials/linkedin.png';
-import medium from '../assets/socials/medium.png';
-import twitter from '../assets/socials/twitter.png';
+import Info from './Info';
 
 function Main() {
   return (
     <main className="main-mobile">
-      <section className="top-headers">
-        <h1>Mohamed Amine Hajltaief</h1>
-        <span>FULL STACK WEB DEVELOPPER</span>
-      </section>
-
-      <section className="self-info">
-        <div className="img-container">
-          <img src={myPic} alt="my-pfp" />
-        </div>
-        <div className="info-container">
-          <p className="info-intro">
-            I can help you build a product , feature or website Look through
-            some of my work and experience!
-            If you like what you see and have a project you need coded, don&apos;t
-            hestiate to contact me.
-          </p>
-          <ul className="social-media-header d-flex">
-            <li><a rel="noopener noreferrer" target="_blank" href="https://twitter.com/Amino47612441"><img src={twitter} alt="twitter" /></a></li>
-            <li><a rel="noopener noreferrer" target="_blank" href="https://www.linkedin.com/in/mohamedamine-hajltaief/"><img src={linkedin} alt="linkedin" /></a></li>
-            <li><a rel="noopener noreferrer" target="_blank" href="https://medium.com/@aminobhl"><img src={medium} alt="medium" /></a></li>
-            <li><a rel="noopener noreferrer" target="_blank" href="https://github.com/AmineHLub"><img src={github} alt="github" /></a></li>
-            <li><a rel="noopener noreferrer" target="_blank" href="https://angel.co/u/mohamed-amine-hajltaief"><img src={angel} alt="angelist" /></a></li>
-          </ul>
-        </div>
-      </section>
-      <div className="seperator" />
+      <Info />
     </main>
   );
 }
