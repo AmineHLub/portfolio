079e70efd37fcd27c012c56e429758a3e57630c4
AmineHLub <Amino.bhl@gmail.com>
2022-03-09 15:41:11 +0100

    Add about me and contact sections

--- src/components/About.js
+++ src/components/About.js
@@ -3,6 +3,18 @@ import '../mobile-styling/about.css';
 
 export default function About() {
   return (
-    <div className="about-me">About</div>
+    <section className="about-me-container d-flex">
+      <h2>About</h2>
+      <div className="info-and-resume">
+        <p>
+          I can help you build a product , feature or
+          website Look through some of my work and experience!
+          If you like what you see and have a project
+          you need coded, don&apos;t hestiate to contact me.
+        </p>
+        <a rel="noopener noreferrer" target="_blank" href="https://docs.google.com/document/d/1QCCGYmClCusXQpCrbPcAi_CyktSNMAJUyRjZCRZOdrc/edit">Resume</a>
+      </div>
+      <div className="seperator" />
+    </section>
   );
 }
---
+++ src/components/Contact.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+export default function Contact() {
+  return (
+    <section className="contact-container d-flex" id="contact">
+      <h2>Contact me</h2>
+      <div className="contact-header">
+        <p>
+          I&apos;m always interested in hearing about new projects,
+          so if you&apos;d like to chat please get in touch.
+        </p>
+      </div>
+      <form action="https://formspree.io/f/xayvjvar" method="POST">
+        <div className="form-container d-flex">
+          <input type="text" className="input input-name" placeholder="Full name" maxLength="30" name="username" required />
+          <input type="email" className="input input-email" placeholder="Email address" name="_replyto" />
+          <textarea className="input input-message" placeholder="Type your message here..." minLength="5" maxLength="500" name="message" required />
+          <button className="button-submit" type="submit">Get in touch</button>
+        </div>
+      </form>
+      <div className="seperator last-seperator" />
+    </section>
+  );
+}
