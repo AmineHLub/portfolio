57d7cbfb804de116c336bf5266ecb4f0d0d3bc59
AmineHLub <Amino.bhl@gmail.com>
2022-03-10 18:09:12 +0100

    Add responsive Slideshow feature to its settings

--- src/components/SlideShow.js
+++ src/components/SlideShow.js
@@ -22,8 +22,40 @@ function SlideShow() {
     dots: true,
     infinite: true,
     speed: 700,
-    slidesToShow: 1,
-    slidesToScroll: 1,
+    slidesToShow: 4,
+    slidesToScroll: 2,
+    responsive: [
+      {
+        breakpoint: 520, // mobile
+        settings: {
+          dots: true,
+          infinite: true,
+          speed: 700,
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 850, // tablet
+        settings: {
+          dots: true,
+          infinite: true,
+          speed: 700,
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 980, // l-tablet
+        settings: {
+          dots: true,
+          infinite: true,
+          speed: 700,
+          slidesToShow: 3,
+          slidesToScroll: 1,
+        },
+      },
+    ],
   };
 
   return (
