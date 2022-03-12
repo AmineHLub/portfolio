3cd04726438c173d0683cd2bd394f8c6e989b916
AmineHLub <Amino.bhl@gmail.com>
2022-03-12 12:08:15 +0100

    add conditional navbar rendering and minimum styling to desktop nav

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import './App.css';
+import { useMediaQuery } from 'react-responsive';
 import MobileNavbar from './components/MobileNavbar';
+import Navbar from './components/Navbar';
 import Main from './components/Main';
 
 function App() {
+  const isMobile = useMediaQuery({
+    query: '(max-width: 884px)',
+  });
   return (
     <>
-      <MobileNavbar />
+      {isMobile ? <MobileNavbar /> : <Navbar />}
       <Main />
     </>
   );
